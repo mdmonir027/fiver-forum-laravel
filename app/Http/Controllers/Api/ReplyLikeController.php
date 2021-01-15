6d4855490984be3afa605bf12a3b29689660d600
Md Monir <60777309+mdmonir27@users.noreply.github.com>
2021-01-15 22:21:39 +0600

    Controller Created

---
+++ app/Http/Controllers/Api/ReplyLikeController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\ReplyLike;
+use Illuminate\Http\Request;
+
+class ReplyLikeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\ReplyLike  $replyLike
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ReplyLike $replyLike)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\ReplyLike  $replyLike
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ReplyLike $replyLike)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\ReplyLike  $replyLike
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ReplyLike $replyLike)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\ReplyLike  $replyLike
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ReplyLike $replyLike)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/Api/ReplyReplyController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\ReplyReply;
+use Illuminate\Http\Request;
+
+class ReplyReplyController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\ReplyReply  $replyReply
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ReplyReply $replyReply)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\ReplyReply  $replyReply
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ReplyReply $replyReply)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\ReplyReply  $replyReply
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ReplyReply $replyReply)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\ReplyReply  $replyReply
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ReplyReply $replyReply)
+    {
+        //
+    }
+}
