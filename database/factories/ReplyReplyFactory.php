487415dedbe4b18c1225dfc7b20dd11f93c2d4b1
Md Monir <60777309+mdmonir27@users.noreply.github.com>
2021-01-15 21:54:39 +0600

    Create Factories

---
+++ database/factories/ReplyReplyFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\ReplyReply;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ReplyReplyFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = ReplyReply::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
